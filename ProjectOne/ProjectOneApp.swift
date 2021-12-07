e292c5961a7cc23d44928ae9bd8243eb7ff97a78
citixenken <kmuyesu@gmail.com>
2021-12-07 17:16:35 +0300

    first commit

---
+++ ProjectOne/ProjectOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOneApp.swift
+//  ProjectOne
+//
+//  Created by Ken Muyesu on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
